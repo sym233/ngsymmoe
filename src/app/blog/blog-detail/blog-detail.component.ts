cccab31e60799d2a4170ce86f8ef57729596400e
sym233 <849228735@qq.com>
2020-05-15 16:50:02 +0800

    apply angular material; merge header and nav; add side nav for handset; add nav icons.

--- src/app/blog/blog-detail/blog-detail.component.ts
+++ src/app/blog/blog-detail/blog-detail.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import { Observable } from 'rxjs';
 
 import { BlogService } from '../blog.service';
 
@@ -40,11 +39,9 @@ export class BlogDetailComponent implements OnInit {
         );
       }
     });
-
   }
 
   backBlogPage() {
     this.router.navigate(['..']);
   }
-
 }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-nav',
@@ -6,21 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.less']
 })
 export class NavComponent implements OnInit {
-
+  openSideNav = false;
+  isHandset = false;
   navLinks = [{
     name: 'Home',
     path: 'home',
+    icon: 'home',
   }, {
     name: 'Blog',
     path: 'blog',
+    icon: 'library_books',
   }, {
     name: 'About',
     path: 'about',
+    icon: 'portrait',
   }];
 
-  constructor() { }
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.isHandset$.subscribe(isHandset => {
+      this.openSideNav = false;
+      this.isHandset = isHandset;
+    });
   }
-
 }
