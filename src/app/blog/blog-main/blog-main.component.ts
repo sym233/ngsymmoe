e21ac2b467b0ee2f0df67bea8aa36d1ac18690c0
sym233 <849228735@qq.com>
2020-05-18 16:47:26 +0800

    add blog list loading animation effect

--- src/app/blog/blog-main/blog-main.component.ts
+++ src/app/blog/blog-main/blog-main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 import { BlogService } from '../blog.service';
 import { BlogIndex } from '../blogIndex';
@@ -6,7 +7,36 @@ import { BlogIndex } from '../blogIndex';
 @Component({
   selector: 'app-blog-main',
   templateUrl: './blog-main.component.html',
-  styleUrls: ['./blog-main.component.less']
+  styleUrls: ['./blog-main.component.less'],
+  animations: [
+    trigger('blogLoadHint', [
+      state('in', style({
+        transform: 'none',
+        opacity: 1,
+      })),
+      state('void', style({
+        opacity: 0,
+      })),
+      transition(':leave', [
+        animate(700, style({
+          transform: 'translateY(-5em)',
+        })),
+      ]),
+    ]),
+    trigger('blogList', [
+      state('in', style({
+        transform: 'none',
+        opacity: 1,
+      })),
+      state('void', style({
+        transform: 'translateY(5em)',
+        opacity: 0,
+      })),
+      transition(':enter', [
+        animate('700ms ease-out'),
+      ]),
+    ])
+  ],
 })
 export class BlogMainComponent implements OnInit {
   blogIndex: BlogIndex;
